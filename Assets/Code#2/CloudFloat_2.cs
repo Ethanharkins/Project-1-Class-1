893acec6c664aab3f4f270c3ed6ca1a33cba348c
Ethanharkins <eharkins@ltu.edu>
2022-09-14 11:05:26 -0400

    AddedAssets

---
+++ Assets/Code#2/CloudFloat_2.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudFloat_2 : MonoBehaviour
+{
+     public float speed = 0.2F;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y + (speed * Time.deltaTime), this.transform.position.z);
+    }
+}
